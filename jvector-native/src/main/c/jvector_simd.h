4d288f8c8f66cf7750659a24aac1b4a4c14dc9d4
Raghuveer Devulapalli <raghuveer.devulapalli@ibm.com>
2026-05-12 06:18:10 +0000

    Convert SIMD build infrastructure from C to C++/Highway
    - Delete jvector_simd.c and jvector_simd_check.c (old C implementation)
    - Add jvector_simd_check.cpp (Highway-aware capability detection)
    - Update jvector_simd.h: add Highway includes, new function declarations
      for FP32/PQ/NVQ kernels, remove legacy PanamaVector-specific signatures
    - Update jextract_vector_simd.sh: compile as C++17, link Highway, run
      jextract against the new .cpp translation unit

--- jvector-native/src/main/c/jvector_simd.h
+++ jvector-native/src/main/c/jvector_simd.h
@@ -15,23 +15,46 @@
  */
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #ifndef VECTOR_SIMD_DOT_H
 #define VECTOR_SIMD_DOT_H
 
-// check CPU support
-bool check_avx512_compatibility(void);
 
-//F32
-float dot_product_f32(int preferred_size, const float* a, int aoffset, const float* b, int boffset, int length);
-float euclidean_f32(int preferred_size, const float* a, int aoffset, const float* b, int boffset, int length);
-void bulk_quantized_shuffle_dot_f32_512(const unsigned char* shuffles, int codebookCount, const char* quantizedPartials, float delta, float minDistance, float* results);
-void bulk_quantized_shuffle_euclidean_f32_512(const unsigned char* shuffles, int codebookCount, const char* quantizedPartials, float delta, float minDistance, float* results);
-void bulk_quantized_shuffle_cosine_f32_512(const unsigned char* shuffles, int codebookCount, const char* quantizedPartialSums, float sumDelta, float minDistance, const char* quantizedPartialMagnitudes, float magnitudeDelta, float minMagnitude, float queryMagnitudeSquared, float* results);
-float assemble_and_sum_f32_512(const float* data, int dataBase, const unsigned char* baseOffsets, int baseOffsetsOffset, int baseOffsetsLength);
-float pq_decoded_cosine_similarity_f32_512(const unsigned char* baseOffsets, int baseOffsetsOffset, int baseOffsetsLength, int clusterCount, const float* partialSums, const float* aMagnitude, float bMagnitude);
-void calculate_partial_sums_dot_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums);
-void calculate_partial_sums_euclidean_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums);
-void calculate_partial_sums_best_dot_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums, float* partialBestDistances);
-void calculate_partial_sums_best_euclidean_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums, float* partialBestDistances);
+// APIs exposed to Java via FFI
+#ifdef __cplusplus
+extern "C" {
 #endif
+bool check_avx512_compatibility(void);
+/* PQ kernels */
+float assemble_and_sum_f32_512(const float* data, int dataBase, const unsigned char* baseOffsets, int baseOffsetsOffset, size_t baseOffsetsLength);
+float assemble_and_sum_pq_f32(const float* data, size_t subspaceCount, const unsigned char* baseOffsets1, int baseOffsetsOffset1, const unsigned char* baseOffsets2, int baseOffsetsOffset2, int clusterCount);
+float pq_decoded_cosine_similarity_f32_512(const unsigned char* baseOffsets, int baseOffsetsOffset, int baseOffsetsLength, int clusterCount, const float* partialSums, const float* aMagnitude, float bMagnitude);
+void calculate_partial_sums_euclidean_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums);
+void calculate_partial_sums_dot_f32_512(const float* codebook, int codebookBase, int size, int clusterCount, const float* query, int queryOffset, float* partialSums);
+
+/* Vector similarity kernels */
+float dot_product_f32_512_native(const float* a, size_t aoffset, const float* b, size_t boffset, size_t length);
+float cosine_f32_512_native(const float* a, size_t aoffset, const float* b, size_t boffset, size_t length);
+float euclidean_f32_512_native(const float* a, size_t aoffset, const float* b, size_t boffset, size_t length);
+
+/* NVQ kernels */
+void    nvq_quantize_8bit(const float* vector, int length, float alpha, float x0, float minValue, float maxValue, unsigned char* destination);
+float   nvq_loss(const float* vector, int length, float alpha, float x0, float minValue, float maxValue, int nBits);
+float   nvq_uniform_loss(const float* vector, int length, float minValue, float maxValue, int nBits);
+float   nvq_square_l2_distance_8bit(const float* vector, const unsigned char* quantized, int length, float alpha, float x0, float minValue, float maxValue);
+float   nvq_dot_product_8bit(const float* vector, const unsigned char* quantized, int length, float alpha, float x0, float minValue, float maxValue);
+int64_t nvq_cosine_8bit_packed(const float* vector, const unsigned char* quantized, int length, float alpha, float x0, float minValue, float maxValue, const float* centroid);
+
+/* Element-wise in-place vector arithmetic */
+void    add_in_place_f32(float* v1, const float* v2, size_t length);
+void    add_scalar_in_place_f32(float* v1, float value, size_t length);
+void    sub_in_place_f32(float* v1, const float* v2, size_t length);
+void    sub_scalar_in_place_f32(float* v1, float value, size_t length);
+float   max_f32(const float* v, size_t length);
+void    min_in_place_f32(float* v1, const float* v2, size_t length);
+#ifdef __cplusplus
+}
+#endif // extern "C"
+#endif // VECTOR_SIMD_DOT_H
